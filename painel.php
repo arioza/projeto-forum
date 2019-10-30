133581a08f4bd3243301539a5b06719223e664bc
Gabriel de Alencar Arioza <gabriel_alencar95@hotmail.com>
2019-10-30 17:10:07 -0400

    Aciona modal ao clicar no botão que acessa os fóruns

--- painel.php
+++ painel.php
@@ -28,7 +28,7 @@ include('verifica_login.php');
 							<div class="dropdown-menu">
 								<a class="dropdown-item" href="" onclick="dev()"><i class="fas fa-user"></i> Editar Perfil</a>
 								<a class="dropdown-item" href="" onclick="dev()"><i class="fas fa-book-reader"></i> Meus Forúns</a>
-								<a class="dropdown-item" data-toggle="modal" data-target="#ExemploModalCentralizado"><i class="fas fa-door-open"></i> Sair</a>
+								<a class="dropdown-item" data-toggle="modal" data-target="#sair"><i class="fas fa-door-open"></i> Sair</a>
 							</div>
 						</li>
 						<li class="nav-item" style="width: 17%; text-align: center;">
@@ -42,7 +42,7 @@ include('verifica_login.php');
 						</li>
 						<form class="form-inline my-2 my-lg-0" style="width: 25%;">
 							<input class="form-control mr-sm-2" type="search" placeholder="Pesquisar fóruns" aria-label="Pesquisar">
-							<button class="btn btn-outline-primary my-2 my-sm-0" type="submit"><i class="fas fa-search"></i></button>
+							<button class="btn btn-outline-primary my-2 my-sm-0" type="submit"><i class="fas fa-search"  onclick="dev()"></i></button>
 						</form>
 					</ul>				
 				</div>
@@ -55,11 +55,25 @@ include('verifica_login.php');
 				<div class="col-md-7">
 					<h6>Fóruns</h6>
 					<hr style="width: 50%; margin-top: -0px">
-					<a href="foruns/tecnologia.php">Fórum Tecnologia</a>
+					<a class="dropdown-item" data-toggle="modal" data-target="#tecnologia" href="foruns/tecnologia.php">Fórum Tecnologia</a>
 				</div>
 				<div class="col-md-2" style="border-left: solid 1px #DEE1E6">
 					<h6>Google Ads</h6>
 					<hr style="width: 60%; margin-top: -0px">
+					<div style="width: 100%; border-style: solid 1px">
+						<p style="font-size: 12px">Área para propaganda</p>
+						<p style="font-size: 12px">Área para propaganda</p>
+						<p style="font-size: 12px">Área para propaganda</p>
+						<p style="font-size: 12px">Área para propaganda</p>
+						<p style="font-size: 12px">Área para propaganda</p>
+						<p style="font-size: 12px">Área para propaganda</p>
+						<p style="font-size: 12px">Área para propaganda</p>
+						<p style="font-size: 12px">Área para propaganda</p>
+						<p style="font-size: 12px">Área para propaganda</p>
+						<p style="font-size: 12px">Área para propaganda</p>
+						<p style="font-size: 12px">Área para propaganda</p>
+						<p style="font-size: 12px">Área para propaganda</p>
+					</div>
 				</div>
 			</div>
 			<hr>
@@ -67,8 +81,8 @@ include('verifica_login.php');
 		</div>
 	</div>
 
-<!--Inicio Modal -->
-	<div class="modal fade" id="ExemploModalCentralizado" tabindex="-1" role="dialog" aria-labelledby="TituloModalCentralizado" aria-hidden="true">
+<!--Inicio Modal Sair -->
+	<div class="modal fade" id="sair" tabindex="-1" role="dialog" aria-labelledby="TituloModalCentralizado" aria-hidden="true">
 		<div class="modal-dialog modal-dialog-centered" role="document">
 			<div class="modal-content">
 				<div class="modal-header">
@@ -82,6 +96,27 @@ include('verifica_login.php');
 		</div>
 	</div>
 <!--Fim Modal-->
+
+<!--Inicio Modal Fórum Tecnologia -->
+	<div class="modal fade" id="tecnologia" tabindex="-1" role="dialog" aria-labelledby="TituloModalCentralizado" aria-hidden="true">
+		<div class="modal-dialog modal-dialog-centered" role="document">
+			<div class="modal-content">
+				<div class="modal-header">
+					<h5 class="modal-title" id="TituloModalCentralizado">Tecnologia</h5>
+				</div>
+				<div class="modal-content">
+					<p style="text-align: justify; padding-top: 5px">O Fórum de Tecnologia é destinado a tratar assuntos especificamentes sobre tecnologia.</p>
+					<p style="text-align: justify;">Tecnologia é um termo que envolve o conhecimento técnico e científico e a aplicação deste conhecimento através de sua transformação no uso de ferramentas, processos e materiais criados e utilizados a partir de tal conhecimento</p>
+				</div>
+				<div class="modal-footer">
+					<button type="button" class="btn btn-secondary" data-dismiss="modal">Fechar</button>
+					<button type="button" class="btn btn-primary"><a href="foruns/tecnologia.php" style="color: white">Acessar</a></button>
+				</div>
+			</div>
+		</div>
+	</div>
+<!--Fim Modal-->
+
 	<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
 	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
 	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
